46fcce9bd357e0751a80fb243c1224e17e1cbac1
s6fikass <s6fikass@uni-bonn.de>
2018-11-22 00:30:20 +0100

    ...

--- test.py
+++ test.py
@@ -2,5 +2,16 @@
 #
 from corpus.textdata import TextData
 #
-textdata = TextData("data/kvret_train_public.json","data/kvret_dev_public.json","data/kvret_test_public.json")
+glove_filename = "data/samples/jointEmbedding.txt"
 
+textdata = TextData("data/kvret_train_public.json", "data/kvret_dev_public.json",
+                    "data/kvret_test_public.json", glove_filename)
+
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+x = textdata.word_to_embedding_dict["parking_garage"].reshape(1, -1)
+y = textdata.word_to_embedding_dict["dish_parking"].reshape(1, -1)
+y2 = textdata.word_to_embedding_dict["clear_skies"].reshape(1, -1)
+
+
+print(cosine_similarity(x,y))
