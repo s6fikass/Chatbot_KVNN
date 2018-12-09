bbd83ab3e779c0263ddfbfe002fdf01c1811a51c
s6fikass <s6fikass@uni-bonn.de>
2018-12-09 12:23:19 +0100

    add entity lookup and correct entity loss

--- util/utils.py
+++ util/utils.py
@@ -70,7 +70,7 @@ def compute_ent_loss(embedding, logits, target, ent_mask, target_mask):
 
     target_emb = embedding(target_ent)  # B X S X E
 
-    entity_loss = (1 - cosine_loss(target_emb, pred_emb))* ent_mask.type(torch.Tensor)  # B X S
+    entity_loss = (1 - cosine_loss(target_emb, pred_emb))* ent_mask  # B X S
     entity_loss = entity_loss * target_mask
 
     length = torch.sum(target_mask, dim=1)
